f448f2a7bddba1805c74514a86333fc3290f8396
Roar Brænden <roar.brenden.no@gmail.com>
2024-03-04 15:51:18 +0100

    Complete web.xml and upgrade versions

--- examples/security-tomcat-user-identitystore/src/main/java/org/superbiz/movie/Api.java
+++ examples/security-tomcat-user-identitystore/src/main/java/org/superbiz/movie/Api.java
@@ -16,11 +16,9 @@
  */
 package org.superbiz.movie;
 
-import jakarta.annotation.security.DeclareRoles;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/api")
-@DeclareRoles({"admin"})
 public class Api extends Application {
 }
